a1f64bc040d43e0c884e03399262a011cc30da6d
Thawiat00 <Thawiat01@gmail.com>
2026-01-18 17:44:51 +0700

    update comment and object and variable
    update comment and object and variable

---
+++ topic/1.2 — Comments/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+
+	/* To calculate the final grade, we sum all the weighted midterm and homework scores
+	and then divide by the number of scores to assign a percentage, which is
+	used to calculate a letter grade. */
+
+
+	std::cout << "Comment Section!";
+	return 0;
+}
+
+
